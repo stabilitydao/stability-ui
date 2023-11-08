4ab2ef2addf6619817c6db9022cd312abb19d7fa
RenzouM <renzoumgmail.com>
2023-11-08 01:36:06 -0300

    tabs, select asset, input added

--- src/components/Vault.tsx
+++ src/components/Vault.tsx
@@ -1,4 +1,5 @@
-import type React from "react";
+/** @format */
+
 import { useEffect, useState } from "react";
 import { useStore } from "@nanostores/react";
 import { readContract } from "viem/actions";
@@ -11,8 +12,7 @@ import {
 } from "wagmi";
 import VaultAbi from "../abi/VaultAbi";
 import StrategyAbi from "../abi/StrategyAbi";
-import { platform } from "../constants";
-import data from "../stability.tokenlist.json";
+import tokensJson from "../stability.tokenlist.json";
 import type { Token, assetPrices } from "../types";
 import { formatUnits } from "viem";
 
@@ -38,20 +38,15 @@ export function addAssetsPrice(data: any) {
 }
 
 export default function Vault(props: Props) {
+  const vaultt: `0x${string}` | undefined = props.vault;
   const $assetsPrices = useStore(assetsPrices);
   const vault = useStore(vaultData);
   const strategy = useStore(assets);
-  console.log(vault);
-
-  console.log(strategy);
-  console.log(props.vault);
-  console.log(data.tokens);
-  console.log($assetsPrices);
-
-  const vaultt: `0x${string}` | undefined = props.vault;
-  console.log(vaultt);
-
   const _publicClient = usePublicClient();
+  const [option, setOption] = useState("");
+  const [defaultOption, setDefaultOption] = useState("");
+  const [defaultOptionValue, setDefaultOptionValue] = useState("");
+  const [tab, setTab] = useState("Deposit");
 
   useEffect(() => {
     async function getStrategy() {
@@ -72,15 +67,46 @@ export default function Vault(props: Props) {
 
           if (Array.isArray(ss)) {
             assets.set(ss);
+            defaultAssetsOption(ss);
             console.log("assets", ss);
+            console.log(option);
           } else {
             console.error("ss is not an array");
           }
         }
       }
     }
+
     getStrategy();
   }, [vaultt]);
+
+  //Default assets strategy on <select>
+  function defaultAssetsOption(ss: string[]) {
+    const strategyAssets = ss;
+    const defaultOption: string[] = [];
+
+    for (let i = 0; i < strategyAssets.length; i++) {
+      const token = tokensJson.tokens.find(
+        token => strategyAssets[i] === token.address
+      );
+
+      if (token) {
+        defaultOption[i] = token.symbol;
+      } else {
+        defaultOption[i] = "Token not found.";
+      }
+    }
+
+    const defaultOptionToString = defaultOption.join(" + ");
+    setDefaultOption(defaultOptionToString);
+    setDefaultOptionValue(ss.join(", "));
+    setOption(ss.join(", "));
+  }
+  //Default assets strategy on <select>
+
+  function changeOption(e: string) {
+    setOption(e);
+  }
 
   if (props.vault && vault[props.vault]) {
     return (
@@ -103,6 +129,255 @@ export default function Vault(props: Props) {
             </tr>
           </tbody>
         </table>
+        <div
+          style={{
+            width: "531px",
+            justifyContent: "center",
+            display: "grid",
+            margin: "auto",
+            gridGap: 0,
+            marginBottom: "50px",
+            borderStyle: "solid",
+            borderWidth: "1px",
+            height: "auto",
+            marginTop: "20px",
+          }}>
+          <div
+            style={{
+              display: "flex",
+            }}>
+            <button
+              style={{
+                width: "100%",
+                height: "65px",
+                fontSize: "23px",
+              }}
+              onClick={() => setTab("Deposit")}>
+              Deposit
+            </button>
+
+            <button
+              style={{
+                width: "100%",
+                height: "65px",
+                fontSize: "23px",
+              }}
+              onClick={() => setTab("Withdraw")}>
+              Withdraw
+            </button>
+          </div>
+          <form
+            style={{
+              width: "531px",
+              margin: "auto",
+              display: "grid",
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: "18px",
+            }}>
+            <div
+              style={{
+                display: "grid",
+                justifyContent: "center",
+                width: "330px",
+                margin: "auto",
+                marginTop: "10px",
+              }}>
+              <label style={{ color: "grey", width: "120px" }}>
+                Select token
+              </label>
+              <select
+                onChange={e => changeOption(e.target.value)}
+                style={{ height: "46px" }}>
+                <option
+                  value={defaultOptionValue}
+                  style={{ textAlign: "center" }}>
+                  {defaultOption}
+                </option>
+                {tokensJson.tokens &&
+                  tokensJson.tokens.slice(0, -2).map(token => {
+                    return (
+                      <option
+                        key={token.address}
+                        value={token.address}
+                        style={{ textAlign: "center" }}>
+                        {token.symbol}
+                        <img
+                          src={token.logoURI}
+                          alt={token.symbol}
+                        />
+                      </option>
+                    );
+                  })}
+              </select>
+            </div>
+            {defaultOptionValue === option ? (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  width: "330px",
+                  margin: "auto",
+                  marginTop: "10px",
+                  alignItems: "bottom",
+                }}>
+                <div style={{ display: "grid" }}>
+                  <label style={{ textAlign: "center", color: "grey" }}>
+                    Available
+                  </label>
+                  <input
+                    list="amount"
+                    id="amount"
+                    name="amount"
+                    placeholder="0"
+                    style={{
+                      height: "40px",
+                      width: "140px",
+                    }}
+                  />
+                </div>
+                {}
+                <div style={{ display: "grid" }}>
+                  <label style={{ textAlign: "center", color: "grey" }}>
+                    Available
+                  </label>
+                  <input
+                    list="amount"
+                    id="amount"
+                    name="amount"
+                    placeholder="0"
+                    style={{
+                      height: "40px",
+                      width: "140px",
+                    }}
+                  />
+                </div>
+              </div>
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  width: "330px",
+                  margin: "auto",
+                  marginTop: "10px",
+                  justifyContent: "center",
+                }}>
+                <div style={{ display: "grid" }}>
+                  <label style={{ textAlign: "center", color: "grey" }}>
+                    Available
+                  </label>
+                  <input
+                    list="amount"
+                    id="amount"
+                    name="amount"
+                    placeholder="0"
+                    style={{ height: "40px" }}
+                  />
+                </div>
+              </div>
+            )}
+            {tab === "Deposit" ? (
+              <button
+                style={{
+                  height: "45px",
+                  fontSize: "28px",
+                  width: "330px",
+                  margin: "auto",
+                  marginTop: "20px",
+                }}>
+                Deposit
+              </button>
+            ) : (
+              <button
+                style={{
+                  height: "45px",
+                  fontSize: "28px",
+                  width: "330px",
+                  margin: "auto",
+                  marginTop: "20px",
+                }}>
+                Withdraw
+              </button>
+            )}
+          </form>
+
+          <section
+            style={{
+              paddingLeft: "50px",
+              paddingRight: "50px",
+              paddingBottom: "20px",
+              opacity: "50%",
+              borderStyle: "solid",
+              borderWidth: "1px",
+            }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <p
+                style={{
+                  padding: "0px",
+                  margin: "0px",
+                }}>
+                DEPOSIT FEE
+              </p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-help-octagon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round">
+                <path
+                  stroke="none"
+                  d="M0 0h24v24H0z"
+                  fill="none"></path>
+                <path d="M12.802 2.165l5.575 2.389c.48 .206 .863 .589 1.07 1.07l2.388 5.574c.22 .512 .22 1.092 0 1.604l-2.389 5.575c-.206 .48 -.589 .863 -1.07 1.07l-5.574 2.388c-.512 .22 -1.092 .22 -1.604 0l-5.575 -2.389a2.036 2.036 0 0 1 -1.07 -1.07l-2.388 -5.574a2.036 2.036 0 0 1 0 -1.604l2.389 -5.575c.206 -.48 .589 -.863 1.07 -1.07l5.574 -2.388a2.036 2.036 0 0 1 1.604 0z"></path>
+                <path d="M12 16v.01"></path>
+                <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
+              </svg>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+              }}>
+              <p
+                style={{
+                  padding: "0px",
+                  margin: "0px",
+                }}>
+                WITHDRAWAL FEE
+              </p>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-help-octagon"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round">
+                <path
+                  stroke="none"
+                  d="M0 0h24v24H0z"
+                  fill="none"></path>
+                <path d="M12.802 2.165l5.575 2.389c.48 .206 .863 .589 1.07 1.07l2.388 5.574c.22 .512 .22 1.092 0 1.604l-2.389 5.575c-.206 .48 -.589 .863 -1.07 1.07l-5.574 2.388c-.512 .22 -1.092 .22 -1.604 0l-5.575 -2.389a2.036 2.036 0 0 1 -1.07 -1.07l-2.388 -5.574a2.036 2.036 0 0 1 0 -1.604l2.389 -5.575c.206 -.48 .589 -.863 1.07 -1.07l5.574 -2.388a2.036 2.036 0 0 1 1.604 0z"></path>
+                <path d="M12 16v.01"></path>
+                <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
+              </svg>
+            </div>
+            <p>
+              The displayed APY accounts for performance fee that is deducted
+              from the generated yield only
+            </p>
+          </section>
+        </div>
 
         <article
           className="Strategy"
@@ -122,15 +397,15 @@ export default function Vault(props: Props) {
             Strategy assets
           </h2>
           {strategy &&
-            strategy.map((strategy, index) => {
-              const strategyData: Token | undefined = data.tokens.find(
+            strategy.map(strategy => {
+              const strategyData: Token | undefined = tokensJson.tokens.find(
                 token => token.address === strategy
               );
 
               if (strategyData && $assetsPrices) {
                 return (
                   <article
-                    key={index}
+                    key={strategy}
                     style={{
                       padding: "15px",
                       paddingLeft: "20px",
@@ -162,11 +437,11 @@ export default function Vault(props: Props) {
                           style={{
                             textDecoration: "none",
                             color: "white",
-                            margin: "auto",
                             display: "felx",
+                            marginRight: "5px",
                             alignItems: "center",
                           }}>
-                          Vault Adress
+                          Contract
                         </a>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -216,27 +491,17 @@ export default function Vault(props: Props) {
                       </div>
                       <section
                         style={{ paddingLeft: "100px", paddingRight: "50px" }}>
-                        <p style={{ color: "grey" }}>
-                          Lorem ipsum dolor sit amet consectetur, adipisicing
-                          elit. Delectus, molestiae officiis. Ducimus dolore
-                          fuga quod facilis beatae ut nisi magni harum sit non
-                          perferendis, reiciendis rerum illo id placeat saepe!
-                        </p>
+                        <p style={{ color: "grey" }}></p>
                       </section>
                     </div>
                     <div>
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Delectus, molestiae officiis. Ducimus dolore fuga
-                        quod facilis beatae ut nisi magni harum sit non
-                        perferendis, reiciendis rerum illo id placeat saepe!
-                      </p>
+                      <p></p>
                     </div>
                   </article>
                 );
               }
 
-              return null; // Otra opción si no se encuentra la estrategia
+              return null;
             })}
         </article>
       </>
