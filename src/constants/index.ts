b7f94728eba71a32e16b40d68141a4e8cde3791b
Alien Deployer <m@a17.name>
2023-12-10 00:40:25 +0300

    assets-proportion on vaults, asset color

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -39,6 +39,7 @@ const TOKENS_ASSETS = [
       "USDC is a fully-reserved stablecoin, which is a type of cryptocurrency, or digital asset. Unlike other cryptocurrencies that fluctuate in price, USDC is designed to maintain price equivalence to the US dollar.",
     website: "https://www.circle.com/en/usdc",
     docs: "https://developers.circle.com/stablecoins/docs",
+    color: "#3b87df",
   },
   {
     symbol: "USDT",
@@ -47,6 +48,7 @@ const TOKENS_ASSETS = [
       "Tether (USDT) is a cryptocurrency with a value meant to mirror the value of the U.S. dollar. The idea was to create stable digital cash. Tether converts cash into digital currency, to anchor or “tether” the value of the coin to the price of national currencies like the US dollar, the Euro, and the Yen.",
     website: "https://tether.to/en/",
     docs: "https://tether.to/en/knowledge-base/",
+    color: "#5bc7af",
   },
   {
     symbol: "DAI",
@@ -55,6 +57,7 @@ const TOKENS_ASSETS = [
       "DAI is an algorithmic stablecoin issued by MakerDAO, an Ethereum-based protocol, that seeks to maintain an exact ratio of one-to-one with the U.S. dollar.",
     website: "https://makerdao.com/",
     docs: "https://docs.makerdao.com/smart-contract-modules/dai-module/dai-detailed-documentation",
+    color: "#f3ba42",
   },
   {
     symbol: "WMATIC",
@@ -63,6 +66,7 @@ const TOKENS_ASSETS = [
       "WMATIC is a wrapped version of MATIC that enables it to be easily used within DeFi.",
     website: "https://polygon.technology/",
     docs: "https://wiki.polygon.technology/",
+    color: "#9663ee",
   },
   {
     symbol: "WETH",
@@ -71,6 +75,7 @@ const TOKENS_ASSETS = [
       "WETH is an ERC-20 token on Ethereum that represents 1 Ether (ETH)",
     website: "https://ethereum.org/en/",
     docs: "https://ethereum.org/en/developers/docs/",
+    color: "#6372a2",
   },
   {
     symbol: "WBTC",
@@ -79,6 +84,7 @@ const TOKENS_ASSETS = [
       "WBTC is an ERC-20 token on the Ethereum blockchain that is pegged to Bitcoin (BTC). WBTC is backed one-to-one with Bitcoin. Before WBTC, the only way to use Bitcoin in a financial transaction was through centralized entities, like centralized exchanges (CEXs).",
     website: "https://wbtc.network/",
     docs: "",
+    color: "#f0a051",
   },
   {
     symbol: "PROFIT",
@@ -87,6 +93,7 @@ const TOKENS_ASSETS = [
       "The native token PROFIT's primary purpose is to represent ownership shares of the Stability protocol. Given PROFIT holders are effectively owners of Stability. Holding the token also allows investors to manage the Stability protocol collectively.",
     website: "https://stabilitydao.org/tokens",
     docs: "https://book.stabilitydao.org/tokens.html#profit",
+    color: "#886ac3",
   },
   {
     symbol: "SDIV",
@@ -95,6 +102,7 @@ const TOKENS_ASSETS = [
       "The SDIV token is intended to distribute the externally generated profit of the organization in the form of dividends.",
     website: "https://stabilitydao.org/tokens",
     docs: "https://book.stabilitydao.org/tokens.html#sdiv",
+    color: "#232323",
   },
 ];
 
