093f3c8b4cc2ce9cc1bf42a5a21a69e1535f00a3
Alien Deployer <m@a17.name>
2023-12-14 01:56:44 +0300

    zap deposit coming soon

--- src/components/Vault/index.tsx
+++ src/components/Vault/index.tsx
@@ -2117,7 +2117,8 @@ function Vault({ vault }: IProps) {
                     </>
                   ) : (
                     <div>
-                      <div>
+                      <div>Coming soon</div>
+                      <div className="hidden">
                         <div className="flex flex-col mt-[15px] text-[15px] w-full">
                           {balances[option[0]] && (
                             <div className="text-left text-[gray] ml-2">
