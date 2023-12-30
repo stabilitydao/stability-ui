af38f0e673b4604b30ef53c0f16d733245ac2c35
devtealeaf <dev0tea0leaf@gmail.com>
2023-12-30 03:15:05 +0200

    Vault: errors(beta)

--- src/components/Vault/index.tsx
+++ src/components/Vault/index.tsx
@@ -5,6 +5,8 @@ import { formatUnits, parseUnits, zeroAddress, maxUint256 } from "viem";
 import { readContract } from "viem/actions";
 import { writeContract } from "@wagmi/core";
 
+import { type WriteContractErrorType } from "viem";
+
 import { usePublicClient } from "wagmi";
 
 import { SettingsModal } from "./SettingsModal";
@@ -124,6 +126,8 @@ function Vault({ vault }: IProps) {
   const [rotation, setRotation] = useState<number>(0);
   const [isRefresh, setIsRefresh] = useState(false);
   const [loader, setLoader] = useState<boolean>(false);
+
+  const [error, setError] = useState<any>(false);
 
   const tokenSelectorRef = useRef<HTMLDivElement>(null);
 
@@ -477,6 +481,7 @@ function Vault({ vault }: IProps) {
 
   const zapApprove = async () => {
     ///// ZAP TOKENS & UNDERLYING TOKENS
+    setError(false);
     const amount = inputs[option[0]]?.amount;
 
     if (option[0] !== underlyingToken?.address) {
@@ -505,10 +510,12 @@ function Vault({ vault }: IProps) {
             }
             setLoader(false);
           }
-        } catch (error) {
+        } catch (err) {
+          err = err as WriteContractErrorType;
+          setError(err.name);
           lastTx.set("No approve hash...");
           setLoader(false);
-          console.error("APPROVE ERROR:", error);
+          console.error("APPROVE ERROR:", err);
         }
       }
     } else {
@@ -533,15 +540,18 @@ function Vault({ vault }: IProps) {
           }
           setLoader(false);
         }
-      } catch (error) {
+      } catch (err) {
+        err = err as WriteContractErrorType;
+        setError(err.name);
         lastTx.set("No approve hash...");
         setLoader(false);
-        console.error("APPROVE ERROR:", error);
+        console.error("APPROVE ERROR:", err);
       }
     }
   };
   const zapDeposit = async () => {
     ///// UNDERLYING
+    setError(false);
     if (underlyingToken?.address === option[0]) {
       try {
         const shares = parseUnits(underlyingShares, 18);
@@ -569,10 +579,12 @@ function Vault({ vault }: IProps) {
           lastTx.set(transaction?.transactionHash);
           setLoader(false);
         }
-      } catch (error) {
+      } catch (err) {
+        err = err as WriteContractErrorType;
+        setError(err.name);
         lastTx.set("No depositAssets hash...");
         setLoader(false);
-        console.error("UNDERLYING DEPOSIT ERROR:", error);
+        console.error("UNDERLYING DEPOSIT ERROR:", err);
       }
     } else {
       try {
@@ -612,10 +624,12 @@ function Vault({ vault }: IProps) {
           lastTx.set(transaction?.transactionHash);
           setLoader(false);
         }
-      } catch (error) {
+      } catch (err) {
+        err = err as WriteContractErrorType;
+        setError(err.name);
         lastTx.set("No deposit hash...");
         setLoader(false);
-        console.error("ZAP DEPOSIT ERROR:", error);
+        console.error("ZAP DEPOSIT ERROR:", err);
       }
     }
   };
@@ -693,6 +707,7 @@ function Vault({ vault }: IProps) {
   };
 
   const withdrawZapApprove = async () => {
+    setError(false);
     try {
       const assetApprove = await writeContract({
         address: vault as TAddress,
@@ -723,10 +738,12 @@ function Vault({ vault }: IProps) {
         }
         setLoader(false);
       }
-    } catch (error) {
+    } catch (err) {
+      err = err as WriteContractErrorType;
+      setError(err.name);
       lastTx.set("No approve hash...");
       setLoader(false);
-      console.error("ZAP ERROR:", error);
+      console.error("ZAP ERROR:", err);
     }
   };
 
@@ -742,6 +759,7 @@ function Vault({ vault }: IProps) {
   /////
 
   const approve = async (asset: TAddress) => {
+    setError(false);
     const needApprove = option.filter(
       (asset: TAddress) =>
         allowance &&
@@ -790,7 +808,10 @@ function Vault({ vault }: IProps) {
           }
           setLoader(false);
         }
-      } catch (error) {
+      } catch (err) {
+        err = err as WriteContractErrorType;
+
+        setError(err.name);
         lastTx.set("No approve hash...");
         const newAllowance = (await readContract(_publicClient, {
           address: asset,
@@ -819,6 +840,7 @@ function Vault({ vault }: IProps) {
   };
 
   const deposit = async () => {
+    setError(false);
     let assets: string[] = [];
     let input: any = [];
 
@@ -849,14 +871,18 @@ function Vault({ vault }: IProps) {
         lastTx.set(transaction?.transactionHash);
         setLoader(false);
       }
-    } catch (error) {
+    } catch (err) {
+      err = err as WriteContractErrorType;
+
+      setError(err.name);
       lastTx.set("No depositAssets hash...");
       setLoader(false);
-      console.error("DEPOSIT ASSETS ERROR:", error);
+      console.error("DEPOSIT ASSETS ERROR:", err);
     }
   };
 
   const withdraw = async () => {
+    setError(false);
     const sharesToBurn = parseUnits(inputs[option[0]]?.amount, 18);
 
     if (!sharesToBurn) return;
@@ -888,10 +914,12 @@ function Vault({ vault }: IProps) {
           lastTx.set(transaction?.transactionHash);
           setLoader(false);
         }
-      } catch (error) {
+      } catch (err) {
+        err = err as WriteContractErrorType;
+        setError(err.name);
         lastTx.set("No withdrawAssets hash...");
         setLoader(false);
-        console.error("WITHDRAW ERROR:", error);
+        console.error("WITHDRAW ERROR:", err);
       }
     } else {
       const optionAmount = Number(inputs[option[0]]?.amount);
@@ -927,10 +955,12 @@ function Vault({ vault }: IProps) {
           lastTx.set(transaction?.transactionHash);
           setLoader(false);
         }
-      } catch (error) {
+      } catch (err) {
+        err = err as WriteContractErrorType;
+        setError(err.name);
         lastTx.set("No withdraw hash...");
         setLoader(false);
-        console.error("WITHDRAW ERROR:", error);
+        console.error("WITHDRAW ERROR:", err);
       }
     }
   };
@@ -2736,6 +2766,90 @@ function Vault({ vault }: IProps) {
                     CONNECT WALLET
                   </button>
                 ))}
+
+              {error && (
+                <div className="bg-[#734950] border border-[#b75457] rounded-sm mt-5 relative">
+                  <div className="px-2 py-2 flex items-center justify-between">
+                    <svg
+                      width="24"
+                      height="21"
+                      viewBox="0 0 24 21"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="mr-2"
+                    >
+                      <path
+                        d="M23.2266 17.7266L13.8516 1.4375C13.1484 0.226562 11.3125 0.1875 10.6094 1.4375L1.23438 17.7266C0.53125 18.9375 1.42969 20.5 2.875 20.5H21.5859C23.0312 20.5 23.9297 18.9766 23.2266 17.7266ZM12.25 14.3281C13.2266 14.3281 14.0469 15.1484 14.0469 16.125C14.0469 17.1406 13.2266 17.9219 12.25 17.9219C11.2344 17.9219 10.4531 17.1406 10.4531 16.125C10.4531 15.1484 11.2344 14.3281 12.25 14.3281ZM10.5312 7.88281C10.4922 7.60938 10.7266 7.375 11 7.375H13.4609C13.7344 7.375 13.9688 7.60938 13.9297 7.88281L13.6562 13.1953C13.6172 13.4688 13.4219 13.625 13.1875 13.625H11.2734C11.0391 13.625 10.8438 13.4688 10.8047 13.1953L10.5312 7.88281Z"
+                        fill="#DE2E2E"
+                      />
+                    </svg>
+                    <p className="text-[18px] text-[#f2aeae]">{error}</p>
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 12 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="cursor-pointer"
+                      onClick={() => setError(false)}
+                    >
+                      <g filter="url(#filter0_i_910_1842)">
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M0.292893 1.70711C-0.097631 1.31658 -0.097631 0.683417 0.292893 0.292893C0.683418 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L6 4.58579L10.2929 0.292893C10.6834 -0.0976311 11.3166 -0.0976311 11.7071 0.292893C12.0976 0.683417 12.0976 1.31658 11.7071 1.70711L7.41421 6L11.7071 10.2929C12.0976 10.6834 12.0976 11.3166 11.7071 11.7071C11.3166 12.0976 10.6834 12.0976 10.2929 11.7071L6 7.41421L1.70711 11.7071C1.31658 12.0976 0.683417 12.0976 0.292893 11.7071C-0.0976311 11.3166 -0.0976311 10.6834 0.292893 10.2929L4.58579 6L0.292893 1.70711Z"
+                          fill="white"
+                        />
+                      </g>
+                      <defs>
+                        <filter
+                          id="filter0_i_910_1842"
+                          x="0"
+                          y="0"
+                          width="14"
+                          height="14"
+                          filterUnits="userSpaceOnUse"
+                          colorInterpolationFilters="sRGB"
+                        >
+                          <feFlood
+                            floodOpacity="0"
+                            result="BackgroundImageFix"
+                          />
+                          <feBlend
+                            mode="normal"
+                            in="SourceGraphic"
+                            in2="BackgroundImageFix"
+                            result="shape"
+                          />
+                          <feColorMatrix
+                            in="SourceAlpha"
+                            type="matrix"
+                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                            result="hardAlpha"
+                          />
+                          <feOffset dx="2" dy="2" />
+                          <feGaussianBlur stdDeviation="1" />
+                          <feComposite
+                            in2="hardAlpha"
+                            operator="arithmetic"
+                            k2="-1"
+                            k3="1"
+                          />
+                          <feColorMatrix
+                            type="matrix"
+                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                          />
+                          <feBlend
+                            mode="normal"
+                            in2="shape"
+                            result="effect1_innerShadow_910_1842"
+                          />
+                        </filter>
+                      </defs>
+                    </svg>
+                  </div>
+                </div>
+              )}
             </form>
           </div>
         </div>
