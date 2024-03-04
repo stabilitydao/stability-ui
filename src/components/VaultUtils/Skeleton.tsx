78bbef18a36e7ba6fa6148c92a2e1e30392e9688
devtealeaf <dev0tea0leaf@gmail.com>
2024-03-04 20:18:13 +0200

    portfolio skeletons

--- src/components/VaultUtils/Skeleton.tsx
+++ src/components/VaultUtils/Skeleton.tsx
@@ -1,18 +1,31 @@
-import ContentLoader from "react-content-loader";
-
 const Skeleton = (props) => {
   return (
-    <ContentLoader
-      speed={2}
-      width={100}
-      height={100}
+    <svg
+      width="100"
+      height="100"
       viewBox="0 0 320 63"
-      backgroundColor="#262830"
-      foregroundColor="#ccb3f3"
+      xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect x="0" y="0" rx="10" ry="10" width="320" height="63" />
-    </ContentLoader>
+      <rect
+        x="0"
+        y="0"
+        rx="10"
+        ry="10"
+        width="320"
+        height="63"
+        fill="#262830"
+      />
+      <rect x="0" y="0" rx="10" ry="10" width="320" height="63" fill="#a995ff">
+        <animate
+          attributeName="opacity"
+          dur="1s"
+          values="0.2; 1; 0.2"
+          repeatCount="indefinite"
+          begin="0.1"
+        />
+      </rect>
+    </svg>
   );
 };
 
