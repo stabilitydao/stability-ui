07ab3a147d0191b7cfde4377bee7a128de40a71f
Alien Deployer <m@a17.name>
2024-10-17 23:49:01 +0300

    Contests appearance

--- src/modules/Users/components/Contests.tsx
+++ src/modules/Users/components/Contests.tsx
@@ -1,36 +1,79 @@
-import { HeadingText } from "@ui";
+import {HeadingText} from "@ui";
 
-import { formatTimestampToDate } from "../functions";
+import {formatTimestampToDate} from "../functions";
+import {YieldContest} from "@stabilitydao/stability";
 
-const Contests = ({ periodsData }): JSX.Element => {
+interface IContestsProps {
+  periodsData: YieldContest[],
+}
+
+const Contests = ({ periodsData }: IContestsProps): JSX.Element => {
   return (
     <>
       <HeadingText text="Contests" scale={2} styles="mb-0" />
-      <div className="flex justify-center gap-10 flex-wrap items-stretch">
+      <div className="flex justify-center flex-wrap items-stretch w-full">
         {!!periodsData.length &&
-          periodsData.map((period, index: number) => (
+          periodsData.map((contest, index: number) => (
             <div
-              key={period.start}
-              className="bg-accent-950 rounded-2xl  min-w-[200px]"
+              key={contest.start}
+              className="w-4/12 p-8"
             >
-              <div className="p-5 flex flex-col gap-3 items-center justify-center text-[18px]">
-                <HeadingText text={period.name} scale={3} />
-                <p>{`${formatTimestampToDate(period.start)} - ${formatTimestampToDate(period.end)}`}</p>
-                <p>
-                  Status:{" "}
-                  {!index ? "Completed" : index === 1 ? "Active" : "Future"}
-                </p>
+              <div className="bg-accent-950 h-full rounded-2xl p-5 flex flex-col gap-3 items-scenter justify-start text-[18px]">
+                <span className="text-[24px] font-bold">{contest.name}</span>
+                <p className="flex items-center text-accent-300 mb-2">
+                  <svg className="mr-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M8 2V6M16 2V6M3 10H21M5 4H19C20.1046 4 21 4.89543 21 6V20C21 21.1046 20.1046 22 19 22H5C3.89543 22 3 21.1046 3 20V6C3 4.89543 3.89543 4 5 4Z"
+                      stroke="#b2a5fd" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                  </svg>
+                  {`${formatTimestampToDate(contest.start)} - ${formatTimestampToDate(contest.end)}`}</p>
+                <div
+                  className={"h-[25px] text-[12px] inline-flex w-[100px] justify-center items-center font-semibold px-[8px] rounded-[100px] mb-2 "
+                    + (
+                      !index ?
+                        "bg-neutral-950 text-neutral-500"
+                        : index === 1
+                          ? "bg-success-950 border-[1px] border-success-400 text-success-400"
+                          : "bg-accent-950 border-[1px] border-accent-400 text-accent-400"
+                    )}
+                >
+                  {!index ? "Ended" : index === 1 ? "Ongoing" : "Future"}
+                </div>
 
-                {!!period.rewards.length &&
-                  period.rewards.map((reward, index: number) => (
-                    <div key={index} className="bg-accent-400 rounded-2xl">
-                      <div className="p-2">
-                        <p>Type: {reward.type}</p>
-                        <p>Winner reward: {reward.winnerReward}</p>
-                        <p>Winners: {reward.winners}</p>
+                {!!contest.rewards.length &&
+                  <div className="flex flex-col">
+                    {contest.rewards.map((reward, index: number) => (
+                      <div
+                        key={index}
+                        className="flex justify-between items-center"
+                      >
+                        <div className="">
+                          {reward.type === "Points" &&
+                              <span className="flex items-center">
+                                  <img src="/pSTBL.svg" className="w-[24px] h-[24px]" alt="pSTBL"/>
+                                  <span className="ml-2  font-bold">pSTBL</span>
+                              </span>
+                          }
+                          {reward.type === "ERC20 Token" &&
+                              <span className="flex items-center">
+                                  todo img
+                                  <span className="ml-1 font-bold">todo symbol</span>
+                              </span>
+                          }
+                          {reward.type === "NFTs" &&
+                              <span className="flex items-center">
+                                  todo nft img
+                                  <span className="ml-1 font-bold">todo nft symbol</span>
+                              </span>
+                          }
+                        </div>
+                        <div>
+                          {reward.winnerReward} x {reward.winners}
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
+                }
               </div>
             </div>
           ))}
--- src/modules/Users/functions/formatTimestampToDate.ts
+++ src/modules/Users/functions/formatTimestampToDate.ts
@@ -1,8 +1,45 @@
 const formatTimestampToDate = (timestamp: number): string => {
   const date = new Date(timestamp * 1000);
   const day = date.getDate();
+  let dayStr
+  if (day === 1 || day === 21 || day === 31) {
+    dayStr = `${day}st`
+  } else if (day === 2 || day === 22) {
+    dayStr = `${day}nd`
+  } else if (day === 3 || day === 23) {
+    dayStr = `${day}rd`
+  } else {
+    dayStr = `${day}th`
+  }
+
   const month = date.getMonth() + 1;
-  return `${day}.${month < 10 ? `0${month}` : month}`;
+  let monthStr
+  if (month === 1) {
+    monthStr = 'Jan'
+  } else if (month === 2) {
+    monthStr = 'Feb'
+  } else if (month === 3) {
+    monthStr = 'Mar'
+  } else if (month === 4) {
+    monthStr = 'Apr'
+  } else if (month === 5) {
+    monthStr = 'May'
+  } else if (month === 6) {
+    monthStr = 'Jun'
+  } else if (month === 7) {
+    monthStr = 'Jul'
+  } else if (month === 8) {
+    monthStr = 'Aug'
+  } else if (month === 9) {
+    monthStr = 'Sept'
+  } else if (month === 10) {
+    monthStr = 'Oct'
+  } else if (month === 11) {
+    monthStr = 'Nov'
+  } else if (month === 12) {
+    monthStr = 'Dec'
+  }
+  return `${dayStr} ${monthStr}`;
 };
 
 export { formatTimestampToDate };
