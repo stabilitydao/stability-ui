a9416b2a183e2679ff02f491f31a7d919c79a819
devtealeaf <dev0tea0leaf@gmail.com>
2024-12-18 15:10:21 +0200

    upd: beta main page

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -2,16 +2,15 @@ import "./home.css";
 
 const Home = (): JSX.Element => {
   return (
-    <div className="w-screen h-screen font-manrope text-[#ECE3DA] flex flex-col home-bg">
-      <div className="mx-5 md:mx-[100px] lg:mx-[150px] my-5 md:my-10 flex flex-col justify-between flex-1">
+    <div className="w-screen h-screen font-manrope font-light text-[#ECE3DA] flex flex-col home-bg">
+      <div className="mx-5 md:mx-[100px] lg:mx-[150px] my-5 md:my-10 flex flex-col justify-between flex-1 z-[2]">
         <header className="flex items-center justify-between flex-wrap gap-5 md:gap-0">
-          <div className="flex items-center gap-2">
-            <img className="w-8 h-8" src="/logo.svg" alt="Stability logo" />
-            <span className="block text-[24px] font-semibold text-[#A995FF]">
-              Stability
-            </span>
-          </div>
-          <div className="flex items-center gap-5 md:gap-[60px] font-medium">
+          <img
+            className="w-[105px] h-[48px] md:w-[175px] md:h-[80px]"
+            src="/full_logo_dark.png"
+            alt="Stability logo"
+          />
+          <div className="items-center gap-5 hidden min-[450px]:flex md:gap-[60px]">
             <a
               className="responsive-font__nav"
               href="https://github.com/stabilitydao"
@@ -43,41 +42,85 @@ const Home = (): JSX.Element => {
           </div>
         </header>
         <main>
-          <h1 className="responsive-font__h1 font-semibold mb-3 leading-[100%]">
+          <h1 className="responsive-font__h1 mb-3 leading-[100%]">
             DeFi Infrastructure Layer
           </h1>
-          <h3 className="responsive-font__h3 opacity-60 font-medium leading-[120%] mb-5 md:mb-10">
-            Stability acts as permissionless, non-custodial and <br /> automatic
-            asset management solution <br /> based on AI.
-          </h3>
+          <p className="responsive-font__p opacity-60 leading-[120%] mb-5 md:mb-[60px]">
+            Stability acts as permissionless, non-custodial and{" "}
+            <br className="" /> automatic asset management solution <br /> based
+            on AI.
+          </p>
           <a
-            className="text-white responsive-font__nav font-semibold leading-[100%] py-[9px] md:py-[18px] px-6 md:px-[48px] bg-[#612FFB] rounded-lg"
+            className="text-white responsive-font__nav leading-[100%] py-[9px] md:py-[18px] px-6 md:px-[48px] bg-[#612FFB] rounded-lg"
             href="/vaults"
           >
             Vaults
           </a>
         </main>
-        <footer className="flex items-center justify-between flex-col lg:flex-row responsive-font__footer">
+        <footer className="flex items-center lg:items-end justify-between flex-col-reverse lg:flex-row md:responsive-font__footer!">
           <a
-            className="flex items-center"
+            className="flex items-center justify-center"
             href="https://www.soniclabs.com/"
             target="_blank"
           >
-            <span>Powered by</span>
-            <img src="/sonic.svg" alt="Sonic" />
+            <span className="mr-[7px] responsive-font__sonic text-nowrap">
+              Powered by
+            </span>
+            <img
+              src="/sonic.svg"
+              alt="Sonic"
+              className="w-[70px] h-[27px] md:w-full md:h-full"
+            />
           </a>
-          <p className="text-right opacity-60 font-medium">
-            Stability is live and already growing.
-            <br /> Join our{" "}
+          <div className="min-[450px]:hidden w-[50%] flex items-center justify-between my-5">
             <a
-              className="text-[#C2AEFF] font-semibold opacity-60"
-              href="https://discord.com/invite/R3nnetWzC9"
+              href="https://github.com/stabilitydao"
               target="_blank"
+              title="GitHub"
             >
-              community
-            </a>{" "}
-            to find out more.
-          </p>
+              <img src="socials/github.svg" alt="GitHub" className="w-5 h-5" />
+            </a>
+            <a href="https://x.com/stabilitydao" target="_blank" title="X">
+              <img src="socials/x.svg" alt="X" className="w-5 h-5" />
+            </a>
+            <a
+              href="https://stabilitydao.gitbook.io/"
+              target="_blank"
+              title="GitBook"
+            >
+              <img
+                src="socials/gitbook.svg"
+                alt="GitBook"
+                className="w-5 h-5"
+              />
+            </a>
+            <a
+              href="https://defillama.com/protocol/stability"
+              target="_blank"
+              title="DefiLlama"
+            >
+              <img
+                src="socials/defillama.svg"
+                alt="DefiLlama"
+                className="w-5 h-5"
+              />
+            </a>
+          </div>
+          <div className="text-center lg:text-right text-[45px] mb-0 lg:mb-[10px]">
+            <p className="hidden lg:block">Join Stability</p>{" "}
+            <p className="text-neutral-400 responsive-font__footer">
+              Stability is live and already growing.
+              <br /> Join our{" "}
+              <a
+                className="text-[#C2AEFF] underline"
+                href="https://discord.com/invite/R3nnetWzC9"
+                target="_blank"
+              >
+                community
+              </a>{" "}
+              to find out more.
+            </p>
+          </div>
         </footer>
       </div>
     </div>
--- src/modules/Vault/components/YieldRates/table/HoldTable.tsx
+++ src/modules/Vault/components/YieldRates/table/HoldTable.tsx
@@ -7,43 +7,49 @@ interface IProps {
 
 const HoldTable: React.FC<IProps> = ({ shareData, holdData }) => {
   return (
-    <table className="table table-auto w-full rounded-lg">
-      <thead className="bg-[#0b0e11]">
-        <tr className="text-[16px] text-[#8f8f8f] uppercase">
-          <th></th>
-          <th>INIT PRICE</th>
-          <th>PRICE</th>
-          <th>CHANGE</th>
-        </tr>
-      </thead>
-      <tbody className="text-[16px]">
-        {!!shareData && (
-          <tr className="hover:bg-[#2B3139]">
-            <td>Vault</td>
-            <td className="text-right py-1">
-              {shareData.sharePriceOnCreation}
-            </td>
-            <td className="text-right py-1">{shareData.sharePrice}</td>
-            <td className="text-right py-1">
-              {Number(shareData.yieldPercent) > 0 && "+"}
-              {shareData.yieldPercent}%
-            </td>
+    <div>
+      <span className="normal-case">
+        VS HODL APR compares depositing tokens into a vault vs holding them in a
+        wallet with it's ratio and timeline.
+      </span>
+      <table className="table table-auto w-full rounded-lg">
+        <thead className="bg-[#0b0e11]">
+          <tr className="text-[16px] text-[#8f8f8f] uppercase">
+            <th></th>
+            <th>INIT PRICE</th>
+            <th>PRICE</th>
+            <th>CHANGE</th>
           </tr>
-        )}
-        {!!holdData &&
-          holdData.map((aprsData) => (
-            <tr key={aprsData.symbol} className="hover:bg-[#2B3139]">
-              <td>{aprsData.symbol}</td>
-              <td className="text-right py-1">{aprsData.initPrice}</td>
-              <td className="text-right py-1">{aprsData.price}</td>
+        </thead>
+        <tbody className="text-[16px]">
+          {!!shareData && (
+            <tr className="hover:bg-[#2B3139]">
+              <td>Vault</td>
               <td className="text-right py-1">
-                {Number(aprsData.priceDifference) > 0 && "+"}
-                {aprsData.priceDifference}%
+                {shareData.sharePriceOnCreation}
+              </td>
+              <td className="text-right py-1">{shareData.sharePrice}</td>
+              <td className="text-right py-1">
+                {Number(shareData.yieldPercent) > 0 && "+"}
+                {shareData.yieldPercent}%
               </td>
             </tr>
-          ))}
-      </tbody>
-    </table>
+          )}
+          {!!holdData &&
+            holdData.map((aprsData) => (
+              <tr key={aprsData.symbol} className="hover:bg-[#2B3139]">
+                <td>{aprsData.symbol}</td>
+                <td className="text-right py-1">{aprsData.initPrice}</td>
+                <td className="text-right py-1">{aprsData.price}</td>
+                <td className="text-right py-1">
+                  {Number(aprsData.priceDifference) > 0 && "+"}
+                  {aprsData.priceDifference}%
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
