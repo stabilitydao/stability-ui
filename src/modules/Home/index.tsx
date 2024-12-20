b2be0b3ed154925970e53bea15d6ee579e6cbb2f
devtealeaf <dev0tea0leaf@gmail.com>
2024-12-20 10:44:05 +0200

    upd: footer

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -6,44 +6,7 @@ const Home = (): JSX.Element => {
   return (
     <div className="w-screen h-screen font-manrope font-light text-[#ECE3DA] flex flex-col home-bg">
       <div className="flex flex-col justify-between flex-1 z-[2]">
-        {/* <header className="flex items-center justify-between flex-wrap gap-5 md:gap-0">
-          <img
-            className="w-[105px] h-[48px] md:w-[175px] md:h-[80px]"
-            src="/full_logo_dark.png"
-            alt="Stability logo"
-          />
-          <div className="items-center gap-5 hidden min-[450px]:flex md:gap-[60px]">
-            <a
-              className="responsive-font__nav"
-              href="https://github.com/stabilitydao"
-              target="_blank"
-            >
-              GitHub
-            </a>
-            <a
-              className="responsive-font__nav"
-              href="https://x.com/stabilitydao"
-              target="_blank"
-            >
-              X
-            </a>
-            <a
-              className="responsive-font__nav"
-              href="https://stabilitydao.gitbook.io/"
-              target="_blank"
-            >
-              Docs
-            </a>
-            <a
-              className="responsive-font__nav"
-              href="https://defillama.com/protocol/stability"
-              target="_blank"
-            >
-              DefiLlama
-            </a>
-          </div>
-        </header> */}
-        <header className="relative flex items-center justify-between ">
+        <header className="relative flex items-center justify-between">
           <img
             className="w-[105px] h-[48px] md:w-[140px] md:h-[60px]"
             src="/full_logo_dark.png"
@@ -85,37 +48,39 @@ const Home = (): JSX.Element => {
             </a>
           </nav>
         </header>
-        <main className="mx-5 md:mx-[100px] lg:mx-[150px] my-5 md:my-10">
-          <a
-            className="flex items-center justify-start"
-            href="https://www.soniclabs.com/"
-            target="_blank"
-          >
-            <span className="mr-[7px] responsive-font__sonic text-nowrap">
-              Powered by
-            </span>
-            <img
-              src="/sonic.svg"
-              alt="Sonic"
-              className="w-[70px] h-[27px] md:w-[109px] md:h-10"
-            />
-          </a>
-          <h1 className="responsive-font__h1 mb-3 leading-[100%]">
-            DeFi Infrastructure Layer
-          </h1>
-          <p className="responsive-font__p opacity-60 leading-[120%] mb-5 md:mb-[60px]">
-            Stability acts as permissionless, non-custodial and{" "}
-            <br className="" /> automatic asset management solution <br /> based
-            on AI.
-          </p>
-          <a
-            className="text-white responsive-font__nav leading-[100%] py-[9px] md:py-[18px] px-6 md:px-[48px] bg-[#612FFB] rounded-lg"
-            href="/vaults"
-          >
-            Vaults
-          </a>
+        <main>
+          <div className="min-[1130px]:min-w-[1095px] min-[1440px]:min-w-[1338px]">
+            <a
+              className="flex items-center justify-start"
+              href="https://www.soniclabs.com/"
+              target="_blank"
+            >
+              <span className="mr-[7px] responsive-font__sonic text-nowrap">
+                Powered by
+              </span>
+              <img
+                src="/sonic.svg"
+                alt="Sonic"
+                className="w-[70px] h-[27px] md:w-[109px] md:h-10"
+              />
+            </a>
+            <h1 className="responsive-font__h1 mb-3 leading-[100%]">
+              DeFi Infrastructure Layer
+            </h1>
+            <p className="responsive-font__p opacity-60 leading-[120%] mb-5 md:mb-[60px]">
+              Stability acts as permissionless, non-custodial and{" "}
+              <br className="" /> automatic asset management solution <br />{" "}
+              based on AI.
+            </p>
+            <a
+              className="text-white responsive-font__nav leading-[100%] py-[9px] md:py-[18px] px-6 md:px-[48px] bg-[#612FFB] rounded-lg"
+              href="/vaults"
+            >
+              Vaults
+            </a>
+          </div>
         </main>
-        <footer className="flex items-center lg:items-end justify-between flex-col lg:flex-row md:responsive-font__footer! gap-5 mx-5 md:mx-[100px] lg:mx-[150px] my-5 md:my-10">
+        <footer className="flex items-center lg:items-end justify-between flex-col lg:flex-row md:responsive-font__footer! gap-5 py-5 px-3">
           <a
             className="flex items-center justify-center"
             href="https://docs.soniclabs.com/funding/airdrop/sonic-boom/winners"
@@ -124,21 +89,28 @@ const Home = (): JSX.Element => {
             <img src="/sonic_boom.png" alt="Sonic" className="" />
           </a>
 
-          <div className="min-[450px]:hidden w-[50%] flex items-center justify-between">
+          <div className="lg:hidden w-[50%] flex items-center justify-center">
             <a
               href="https://github.com/stabilitydao"
               target="_blank"
               title="GitHub"
+              className="px-3 py-2"
             >
               <img src="socials/github.svg" alt="GitHub" className="w-5 h-5" />
             </a>
-            <a href="https://x.com/stabilitydao" target="_blank" title="X">
+            <a
+              href="https://x.com/stabilitydao"
+              target="_blank"
+              title="X"
+              className="px-3 py-2"
+            >
               <img src="socials/x.svg" alt="X" className="w-5 h-5" />
             </a>
             <a
               href="https://stabilitydao.gitbook.io/"
               target="_blank"
               title="GitBook"
+              className="px-3 py-2"
             >
               <img
                 src="socials/gitbook.svg"
@@ -150,6 +122,7 @@ const Home = (): JSX.Element => {
               href="https://defillama.com/protocol/stability"
               target="_blank"
               title="DefiLlama"
+              className="px-3 py-2"
             >
               <img
                 src="socials/defillama.svg"
@@ -172,6 +145,52 @@ const Home = (): JSX.Element => {
               </a>{" "}
               to find out more.
             </p>
+            <div className="items-center justify-end lg:flex hidden">
+              <a
+                href="https://github.com/stabilitydao"
+                target="_blank"
+                title="GitHub"
+                className="px-3 py-2"
+              >
+                <img
+                  src="socials/github.svg"
+                  alt="GitHub"
+                  className="w-4 h-4"
+                />
+              </a>
+              <a
+                href="https://x.com/stabilitydao"
+                target="_blank"
+                title="X"
+                className="px-3 py-2"
+              >
+                <img src="socials/x.svg" alt="X" className="w-4 h-4" />
+              </a>
+              <a
+                href="https://stabilitydao.gitbook.io/"
+                target="_blank"
+                title="GitBook"
+                className="px-3 py-2"
+              >
+                <img
+                  src="socials/gitbook.svg"
+                  alt="GitBook"
+                  className="w-4 h-4"
+                />
+              </a>
+              <a
+                href="https://defillama.com/protocol/stability"
+                target="_blank"
+                title="DefiLlama"
+                className="px-3 py-2"
+              >
+                <img
+                  src="socials/defillama.svg"
+                  alt="DefiLlama"
+                  className="w-4 h-4"
+                />
+              </a>
+            </div>
           </div>
         </footer>
       </div>
