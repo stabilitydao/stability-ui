0021c6e838af54cf458114bc95c69ebe74c33ef2
devtealeaf <dev0tea0leaf@gmail.com>
2025-02-23 17:32:06 +0200

    upd: structure

--- src/modules/Vault/components/InvestForm/SettingsModal.tsx
+++ src/modules/Vault/components/InvestForm/SettingsModal.tsx
@@ -53,27 +53,50 @@ const SettingsModal: React.FC<IProps> = ({
       ref={modalRef}
       className="bg-accent-1000 absolute w-[250px] min-h-[150px] z-20 top-[50px] right-0 rounded-md text-[18px]"
     >
-      <div className="p-3">
-        <p className="uppercase text-[12px] leading-3 text-neutral-500 mb-2">
-          SLIPPAGE (%)
-        </p>
-        <div className="flex items-center gap-2">
-          <input
-            className="bg-accent-900 border-accent-500 w-[80px] py-2 rounded-md pl-5 border h-8"
-            name="Slippage"
-            placeholder="0"
-            value={settingsState.slippage}
-            onChange={handleInputChange}
-          />
+      <div className="p-3 flex flex-col items-start gap-4">
+        <div>
+          <p className="uppercase text-[12px] leading-3 text-neutral-500 mb-2">
+            SLIPPAGE (%)
+          </p>
           <div className="flex items-center gap-2">
-            {TRANSACTION_SETTINGS.slippage.map((value) => (
+            <input
+              className="bg-accent-900 border-accent-500 w-[80px] py-2 rounded-md pl-5 border h-8"
+              name="Slippage"
+              placeholder="0"
+              value={settingsState.slippage}
+              onChange={handleInputChange}
+            />
+            <div className="flex items-center gap-2">
+              {TRANSACTION_SETTINGS.slippage.map((value) => (
+                <div
+                  key={value}
+                  className={`bg-accent-900 w-10 h-8 flex items-center justify-center rounded-md cursor-pointer ${
+                    settingsState.slippage === value &&
+                    "border border-accent-500"
+                  }`}
+                  onClick={() =>
+                    setSettingsState((prev) => ({ ...prev, slippage: value }))
+                  }
+                >
+                  {value}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="uppercase text-[12px] leading-3 text-neutral-500 mb-2">
+            APPROVES
+          </p>
+          <div className="flex items-center gap-2">
+            {TRANSACTION_SETTINGS.approves.map((value) => (
               <div
                 key={value}
-                className={`bg-accent-900 w-10 h-8 flex items-center justify-center rounded-md cursor-pointer ${
-                  settingsState.slippage === value && "border border-accent-500"
+                className={`bg-accent-900 w-auto h-8 flex items-center justify-center rounded-md cursor-pointer p-2 ${
+                  settingsState.approves === value && "border border-accent-500"
                 }`}
                 onClick={() =>
-                  setSettingsState((prev) => ({ ...prev, slippage: value }))
+                  setSettingsState((prev) => ({ ...prev, approves: value }))
                 }
               >
                 {value}
@@ -81,41 +104,25 @@ const SettingsModal: React.FC<IProps> = ({
             ))}
           </div>
         </div>
-        <p className="uppercase text-[12px] leading-3 text-neutral-500 my-2 mt-4">
-          APPROVES
-        </p>
-        <div className="flex items-center gap-3">
-          {TRANSACTION_SETTINGS.approves.map((value) => (
-            <div
-              key={value}
-              className={`bg-accent-900 w-auto h-8 flex items-center justify-center rounded-md cursor-pointer p-2 ${
-                settingsState.approves === value && "border border-accent-500"
-              }`}
-              onClick={() =>
-                setSettingsState((prev) => ({ ...prev, approves: value }))
-              }
-            >
-              {value}
-            </div>
-          ))}
-        </div>
-        <p className="uppercase text-[12px] leading-3 text-neutral-500 my-2 mt-4">
-          GAS LIMIT MULTIPLIER
-        </p>
-        <div className="flex items-center gap-3">
-          {TRANSACTION_SETTINGS.gasLimits.map((value) => (
-            <div
-              key={value}
-              className={`bg-accent-900 w-auto h-8 flex items-center justify-center rounded-md cursor-pointer p-2 ${
-                settingsState.gasLimit === value && "border border-accent-500"
-              }`}
-              onClick={() =>
-                setSettingsState((prev) => ({ ...prev, gasLimit: value }))
-              }
-            >
-              {value}x
-            </div>
-          ))}
+        <div>
+          <p className="uppercase text-[12px] leading-3 text-neutral-500 mb-2">
+            GAS LIMIT MULTIPLIER
+          </p>
+          <div className="flex items-center gap-2">
+            {TRANSACTION_SETTINGS.gasLimits.map((value) => (
+              <div
+                key={value}
+                className={`bg-accent-900 w-auto h-8 flex items-center justify-center rounded-md cursor-pointer p-2 ${
+                  settingsState.gasLimit === value && "border border-accent-500"
+                }`}
+                onClick={() =>
+                  setSettingsState((prev) => ({ ...prev, gasLimit: value }))
+                }
+              >
+                {value}x
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
