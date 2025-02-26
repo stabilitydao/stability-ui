d34e5513c4e6c2153a018da3721c7ac0e373c87c
devtealeaf <dev0tea0leaf@gmail.com>
2025-02-26 11:41:27 +0200

    upd: first drawing

--- src/modules/STBL/index.tsx
+++ src/modules/STBL/index.tsx
@@ -1,4 +1,199 @@
+import { useState, useEffect, useRef } from "react";
+
+import { formatUnits, parseUnits } from "viem";
+
+import { useStore } from "@nanostores/react";
+
+import { writeContract } from "@wagmi/core";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
+
+import { connected, account } from "@store";
+
+import { Loader } from "@ui";
+
+import { getDate } from "@utils";
+
+import { Timer } from "./components";
+
+import {
+  sonicClient,
+  SALE_CONTRACT,
+  SaleABI,
+  ERC20ABI,
+  wagmiConfig,
+} from "@web3";
+
+import type { TAddress } from "@types";
+
 const STBL = (): JSX.Element => {
+  const $connected = useStore(connected);
+  const $account = useStore(account);
+
+  const { open } = useWeb3Modal();
+
+  const USDC = "0x29219dd400f2bf60e5a23d13be72b486d4038894";
+
+  const [balance, setBalance] = useState(1);
+
+  const [saleData, setSaleData] = useState({
+    price: "-",
+    tge: 0,
+    start: 0,
+    end: 0,
+    sold: "-",
+    allocation: "-",
+    bought: "-",
+    raised: "-",
+    maxRaise: "-",
+  });
+
+  const [inputValue, setInputValue] = useState("");
+  const [transactionInProgress, setTransactionInProgress] = useState(false);
+  const [needConfirm, setNeedConfirm] = useState(false);
+  const [button, setButton] = useState("");
+  const [loader, setLoader] = useState<boolean>(false);
+
+  const input = useRef<HTMLInputElement>(null);
+
+  const getAllowance = async () => {
+    const newAllowance = await sonicClient.readContract({
+      address: USDC,
+      abi: ERC20ABI,
+      functionName: "allowance",
+      args: [$account as TAddress, SALE_CONTRACT],
+    });
+
+    console.log(newAllowance);
+  };
+
+  const approve = async () => {
+    try {
+      let sum = parseUnits(100, 6);
+      setNeedConfirm(true);
+      const assetApprove = await writeContract(wagmiConfig, {
+        address: USDC,
+        abi: ERC20ABI,
+        functionName: "approve",
+        args: [SALE_CONTRACT, sum],
+      });
+      setNeedConfirm(false);
+    } catch (error) {
+      console.error("Approve error:", error);
+    }
+  };
+
+  const buy = async () => {};
+
+  const getBalance = async () => {
+    try {
+      const USDCBalance = await sonicClient.readContract({
+        address: USDC,
+        abi: ERC20ABI,
+        functionName: "balanceOf",
+        args: [$account as TAddress],
+      });
+      console.log(USDCBalance);
+    } catch (error) {
+      console.error("Get balance error:", error);
+    }
+  };
+
+  const handleInputChange = (type = "") => {
+    let numericValue = input.current.value.replace(/\D/g, "");
+
+    if (type === "max") {
+      numericValue = balance;
+    }
+
+    setInputValue(numericValue);
+
+    input.current.value = numericValue;
+  };
+
+  const getData = async () => {
+    let bought = "-";
+
+    try {
+      const price = (await sonicClient?.readContract({
+        address: SALE_CONTRACT,
+        abi: SaleABI,
+        functionName: "price",
+      })) as bigint;
+
+      const tge = (await sonicClient?.readContract({
+        address: SALE_CONTRACT,
+        abi: SaleABI,
+        functionName: "tge",
+      })) as bigint;
+
+      const start = (await sonicClient?.readContract({
+        address: SALE_CONTRACT,
+        abi: SaleABI,
+        functionName: "start",
+      })) as bigint;
+
+      const end = (await sonicClient?.readContract({
+        address: SALE_CONTRACT,
+        abi: SaleABI,
+        functionName: "end",
+      })) as bigint;
+
+      const sold = (await sonicClient?.readContract({
+        address: SALE_CONTRACT,
+        abi: SaleABI,
+        functionName: "sold",
+      })) as bigint;
+
+      const allocation = (await sonicClient?.readContract({
+        address: SALE_CONTRACT,
+        abi: SaleABI,
+        functionName: "ALLOCATION_SALE",
+      })) as bigint;
+
+      if ($account) {
+        const userBought = (await sonicClient?.readContract({
+          address: SALE_CONTRACT,
+          abi: SaleABI,
+          functionName: "bought",
+          args: [$account as TAddress],
+        })) as bigint;
+
+        bought = formatUnits(userBought, 18);
+      }
+
+      const raised = String(
+        Number(formatUnits(price, 6)) * Number(formatUnits(sold, 18))
+      );
+
+      const maxRaise = String(
+        Number(formatUnits(price, 6)) * Number(formatUnits(allocation, 18))
+      );
+
+      setSaleData({
+        price: formatUnits(price, 6),
+        tge: Number(tge),
+        start: Number(start),
+        end: Number(end),
+        sold: formatUnits(sold, 18),
+        allocation: formatUnits(allocation, 18),
+        bought,
+        raised,
+        maxRaise,
+      });
+    } catch (error) {
+      console.error("Get data error:", error);
+    }
+  };
+
+  useEffect(() => {
+    if ($account) {
+      getBalance();
+    }
+  }, [$account]);
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="w-full xl:min-w-[1200px] max-w-[1400px] font-manrope">
       <div className="STBL">
@@ -34,6 +229,128 @@ const STBL = (): JSX.Element => {
             </div>
           </div>
           <img src="/STBL.png" alt="STBL" />
+        </div>
+      </div>
+
+      <div>
+        <p>Price: {saleData.price}</p>
+        <p>TGE: {getDate(saleData.tge)}</p>
+        <p>Start: {getDate(saleData.start)}</p>
+        <p>End: {getDate(saleData.end)}</p>
+        <p>Sold: {saleData.sold}</p>
+        <p>Allocation: {saleData.allocation}</p>
+        <p>Bought: {saleData.bought}</p>
+        <p>Raised: {saleData.raised}</p>
+        <p>Max raise: {saleData.maxRaise}</p>
+
+        <Timer start={saleData.start} end={saleData.end} />
+      </div>
+
+      <div className="bg-accent-950 w-[400px] h-[300px] rounded-2xl">
+        <div className="px-5 py-3 flex flex-col justify-between h-full">
+          <div>
+            <label className="relative block h-[40px] w-full md:w-[345px]">
+              <span className="absolute inset-y-0 left-4 flex items-center pointer-events-none">
+                <img
+                  src="https://raw.githubusercontent.com/sushiswap/list/master/logos/token-logos/token/usdc.jpg"
+                  alt="USDC"
+                  title="USDC"
+                  className="w-4 h-4 text-neutral-500 rounded-full"
+                />
+              </span>
+
+              <input
+                list="amount"
+                ref={input}
+                name="amount"
+                placeholder="0"
+                value={inputValue}
+                onChange={() => handleInputChange()}
+                type="text"
+                pattern="^[0-9]*[.,]?[0-9]*$"
+                inputMode="decimal"
+                autoComplete="off"
+                className="min-w-full bg-accent-900 hover:border-accent-500 hover:bg-accent-800 outline-none py-[3px] rounded-2xl border-[2px] border-accent-800 focus:border-accent-500 focus:text-neutral-50 text-neutral-500 transition-all duration-300 h-[36px] my-[2px] pl-10"
+              />
+
+              {!!$connected && !!balance && (
+                <button
+                  type="button"
+                  onClick={() => handleInputChange("max")}
+                  className="absolute inset-y-0 right-1 flex items-center px-3 py-1 text-accent-400 text-[12px] font-semibold"
+                >
+                  Max
+                </button>
+              )}
+            </label>
+            <div
+              className={`h-3 text-[12px] leading-3 text-neutral-500  flex items-center gap-1 ${
+                !!balance ? "" : "opacity-0"
+              }`}
+            >
+              <span>Balance: </span>
+              <span>{!!balance ? balance : "0"}</span>
+            </div>
+          </div>
+
+          <div>
+            {$connected ? (
+              <>
+                {button === "buy" ? (
+                  <button
+                    disabled={transactionInProgress}
+                    className={`w-full max-w-[425px] md:max-w-[350px] flex items-center text-[16px] bg-accent-500 text-neutral-50 font-semibold justify-center py-3 rounded-2xl ${
+                      transactionInProgress
+                        ? "text-neutral-500 bg-neutral-900 flex items-center justify-center gap-2"
+                        : ""
+                    }`}
+                    type="button"
+                    onClick={buy}
+                  >
+                    <p>{needConfirm ? "Confirm in wallet" : "Buy"}</p>
+
+                    {transactionInProgress && <Loader color={"#a6a0b2"} />}
+                  </button>
+                ) : button === "needApprove" ? (
+                  <button
+                    disabled={transactionInProgress}
+                    className={`w-full max-w-[425px] md:max-w-[350px] flex items-center text-[16px] bg-accent-500 text-neutral-50 font-semibold justify-center py-3 rounded-2xl ${
+                      transactionInProgress
+                        ? "text-neutral-500 bg-neutral-900 flex items-center justify-center gap-2"
+                        : ""
+                    }`}
+                    type="button"
+                    onClick={approve}
+                  >
+                    <p>{needConfirm ? "Confirm in wallet" : "Approve"}</p>
+                    {transactionInProgress && <Loader color={"#a6a0b2"} />}
+                  </button>
+                ) : button === "insufficientBalance" ? (
+                  <button
+                    disabled
+                    className="w-full max-w-[425px] md:max-w-[350px] flex items-center justify-center text-[16px] font-semibold text-neutral-500 bg-neutral-900 py-3 rounded-2xl"
+                  >
+                    Insufficient Balance
+                  </button>
+                ) : (
+                  <button
+                    disabled
+                    className="w-full max-w-[425px] md:max-w-[350px] flex items-center justify-center text-[16px] font-semibold text-neutral-500 bg-neutral-900 py-3 rounded-2xl"
+                  >
+                    {loader ? "Loading..." : "Enter Amount"}
+                  </button>
+                )}
+              </>
+            ) : (
+              <button
+                type="button"
+                className="w-full max-w-[425px] md:max-w-[350px] flex items-center text-[16px] bg-accent-500 text-neutral-50 font-semibold justify-center py-3 rounded-2xl"
+                onClick={() => open()}
+              >
+                Connect Wallet
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
---
+++ src/web3/abi/SaleABI.ts
@@ -0,0 +1,301 @@
+export default [
+  {
+    type: "constructor",
+    inputs: [
+      {
+        name: "platform_",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "spendToken_",
+        type: "address",
+        internalType: "address",
+      },
+      {
+        name: "price_",
+        type: "uint256",
+        internalType: "uint256",
+      },
+      {
+        name: "start_",
+        type: "uint64",
+        internalType: "uint64",
+      },
+      {
+        name: "end_",
+        type: "uint64",
+        internalType: "uint64",
+      },
+      {
+        name: "tge_",
+        type: "uint64",
+        internalType: "uint64",
+      },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "ALLOCATION_SALE",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "bought",
+    inputs: [
+      {
+        name: "user",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [
+      {
+        name: "bought",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "burnNotSold",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "buy",
+    inputs: [
+      {
+        name: "amount",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "claim",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "end",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint64",
+        internalType: "uint64",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "platform",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "price",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "receiptToken",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "setupDates",
+    inputs: [
+      {
+        name: "start_",
+        type: "uint64",
+        internalType: "uint64",
+      },
+      {
+        name: "end_",
+        type: "uint64",
+        internalType: "uint64",
+      },
+      {
+        name: "tge_",
+        type: "uint64",
+        internalType: "uint64",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "setupReceiptToken",
+    inputs: [
+      {
+        name: "receiptToken_",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "setupToken",
+    inputs: [
+      {
+        name: "token_",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "sold",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint256",
+        internalType: "uint256",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "spendToken",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "start",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint64",
+        internalType: "uint64",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "tge",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint64",
+        internalType: "uint64",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "token",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "error",
+    name: "AddressEmptyCode",
+    inputs: [
+      {
+        name: "target",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "AddressInsufficientBalance",
+    inputs: [
+      {
+        name: "account",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+  {
+    type: "error",
+    name: "FailedInnerCall",
+    inputs: [],
+  },
+  {
+    type: "error",
+    name: "SafeERC20FailedOperation",
+    inputs: [
+      {
+        name: "token",
+        type: "address",
+        internalType: "address",
+      },
+    ],
+  },
+];
