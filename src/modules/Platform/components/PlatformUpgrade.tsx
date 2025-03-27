1781271d6ee91f51e6afaf7161fadb7336d5967f
devtealeaf <dev0tea0leaf@gmail.com>
2025-03-27 12:10:29 +0200

    upd: platform fix

--- src/modules/Platform/components/PlatformUpgrade.tsx
+++ src/modules/Platform/components/PlatformUpgrade.tsx
@@ -132,7 +132,7 @@ const PlatformUpgrade = (): JSX.Element => {
 
   useEffect(() => {
     fetchPlatformUpdates();
-  }, []);
+  }, [deployments, $publicClient, $platformVersions]);
 
   const explorer = CHAINS.find((chain) => chain.id === "146")?.explorer;
 
