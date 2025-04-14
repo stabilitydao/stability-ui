bdace82757247bcf9f9a22115ba1bb2a60d7f37e
devtealeaf <dev0tea0leaf@gmail.com>
2025-04-14 16:52:10 +0300

    upd: users table limit

--- src/modules/Users/index.tsx
+++ src/modules/Users/index.tsx
@@ -45,55 +45,43 @@ const Users = (): JSX.Element => {
 
   const [activeContest, setActiveContest] = useState(TABLE_TYPES[1]);
 
+  const [activeContestID, setActiveContestID] = useState(currentPeriod);
+
   const [tableStates, setTableStates] = useState(USERS_TABLE);
   const [tableData, setTableData] = useState<TLeaderboard[]>([]);
+
+  const handleActiveContest = (type: string) => {
+    setActiveContest(type);
+
+    if (type === "ACTIVE") {
+      setActiveContestID(currentPeriod);
+    }
+
+    if (type === "PAST") {
+      setActiveContestID(previousPeriod);
+    }
+  };
 
   const initTableData = async () => {
     if (!!allContests.ACTIVE?.length) {
       //@ts-ignore
-      let contestData = allContests[activeContest].sort(
-        (a: TLeaderboard, b: TLeaderboard) => b.earned - a.earned
-      );
-
+      let contestData = allContests[activeContest];
       contestData = contestData.map((data: TLeaderboard, index: number) => ({
         ...data,
         rank: index + 1,
       }));
-
       setTableData(contestData);
     }
   };
 
-  const allContests = useMemo(() => {
-    const absoluteLeaderboard = $apiData?.leaderboards.absolute.map((user) =>
-      user?.points ? user : { ...user, points: 0 }
-    );
-
-    const absoluteLeaderboardMap = absoluteLeaderboard
-      ? Object.fromEntries(
-          absoluteLeaderboard?.map((user) => [user.address, user.points])
-        )
-      : {};
-
-    const fillPoints = (leaderboard?: { address: string; points?: number }[]) =>
-      leaderboard?.map((user) => ({
-        ...user,
-        points: absoluteLeaderboardMap[user?.address] ?? 0,
-      }));
-
-    const pastLeaderboard = fillPoints(
-      $apiData?.leaderboards?.[previousPeriod as keyof YieldContest]
-    );
-    const activeLeaderboard = fillPoints(
-      $apiData?.leaderboards?.[currentPeriod as keyof YieldContest]
-    );
-
-    return {
-      PAST: pastLeaderboard,
-      ACTIVE: activeLeaderboard,
-      ABSOLUTE: absoluteLeaderboard,
-    };
-  }, [$apiData?.leaderboards]);
+  const allContests = useMemo(
+    () => ({
+      PAST: $apiData?.leaderboards?.[previousPeriod as keyof YieldContest],
+      ACTIVE: $apiData?.leaderboards?.[currentPeriod as keyof YieldContest],
+      ABSOLUTE: $apiData?.leaderboards.absolute,
+    }),
+    [$apiData?.leaderboards]
+  );
 
   useEffect(() => {
     initTableData();
@@ -145,7 +133,7 @@ const Users = (): JSX.Element => {
             <p
               key={type}
               className={`w-[150px] md:w-1/3 whitespace-nowrap z-20 text-center p-4 border-b-[1.5px] border-transparent ${hasContestData ? "cursor-pointer" : "cursor-default hover:border-transparent opacity-50"} ${isActive ? "text-neutral-50 !border-accent-500" : "text-neutral-500 hover:border-accent-800"}`}
-              onClick={() => hasContestData && setActiveContest(type)}
+              onClick={() => hasContestData && handleActiveContest(type)}
             >
               {dateRange}
             </p>
@@ -153,55 +141,68 @@ const Users = (): JSX.Element => {
         })}
       </div>
       {tableData.length ? (
-        <div className="overflow-x-auto md:overflow-x-visible md:min-w-[700px]">
-          <table className="w-full font-manrope table table-auto select-none mb-9 min-w-[700px] md:min-w-full">
-            <thead className="bg-accent-950 text-neutral-600 h-[36px]">
-              <tr className="text-[12px] uppercase">
-                {tableStates.map((value: TTableColumn, index: number) => (
-                  <TableColumnSort
-                    key={value.name + index}
-                    index={index}
-                    value={value.name}
-                    sort={sortTable}
-                    table={tableStates}
-                    setTable={setTableStates}
-                    tableData={tableData}
-                    setTableData={setTableData}
-                  />
-                ))}
-              </tr>
-            </thead>
-            <tbody className="text-[14px]">
-              {!!tableData.length &&
-                tableData.map((user: TLeaderboard) => (
-                  <tr
-                    key={user.address}
-                    className="h-[48px] hover:bg-accent-950"
-                  >
-                    <td className="px-4 py-3 text-center sticky md:relative left-0 md:table-cell bg-accent-950 md:bg-transparent z-10">
-                      {user.rank}
-                    </td>
-                    <td
-                      className={`px-4 py-3 text-center ${$account?.toLowerCase() === user.address ? "underline" : ""}`}
-                      style={{ fontFamily: "monospace" }}
+        <>
+          <div className="overflow-x-auto md:overflow-x-visible md:min-w-[700px]">
+            <table className="w-full font-manrope table table-auto select-none mb-9 min-w-[700px] md:min-w-full">
+              <thead className="bg-accent-950 text-neutral-600 h-[36px]">
+                <tr className="text-[12px] uppercase">
+                  {tableStates.map((value: TTableColumn, index: number) => (
+                    <TableColumnSort
+                      key={value.name + index}
+                      index={index}
+                      value={value.name}
+                      sort={sortTable}
+                      table={tableStates}
+                      setTable={setTableStates}
+                      tableData={tableData}
+                      setTableData={setTableData}
+                    />
+                  ))}
+                </tr>
+              </thead>
+              <tbody className="text-[14px]">
+                {!!tableData.length &&
+                  tableData.map((user: TLeaderboard) => (
+                    <tr
+                      key={user.address}
+                      className="h-[48px] hover:bg-accent-950"
                     >
-                      {getShortAddress(user.address, 6, 4)}
-                    </td>
-                    <td className="text-center px-4 py-3">
-                      {user.earned <= 0.01
-                        ? user.earned.toFixed(4)
-                        : user.earned.toFixed(2)}
-                    </td>
-                    <td className="text-center px-4 py-3">
-                      {user.deposit
-                        ? (Math.round(user.deposit * 100) / 100).toFixed(2)
-                        : ""}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
+                      <td className="px-4 py-3 text-center sticky md:relative left-0 md:table-cell bg-accent-950 md:bg-transparent z-10">
+                        {user.rank}
+                      </td>
+                      <td
+                        className={`px-4 py-3 text-center ${$account?.toLowerCase() === user.address ? "underline" : ""}`}
+                        style={{ fontFamily: "monospace" }}
+                      >
+                        {getShortAddress(user.address, 6, 4)}
+                      </td>
+                      <td className="text-center px-4 py-3">
+                        {user.earned <= 0.01
+                          ? user.earned.toFixed(4)
+                          : user.earned.toFixed(2)}
+                      </td>
+                      <td className="text-center px-4 py-3">
+                        {user.deposit
+                          ? (Math.round(user.deposit * 100) / 100).toFixed(2)
+                          : ""}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+
+          {activeContest != "ABSOLUTE" && (
+            <div className="w-full flex items-center justify-center">
+              <a
+                className="bg-accent-900 max-w-[250px] min-w-[100px] text-[14px] font-semibold h-8 md:h-10 sm:py-1 md:px-3 rounded-xl sm:gap-1 flex items-center justify-center w-8 md:w-full"
+                href={`/contests/${activeContestID}`}
+              >
+                All Users
+              </a>
+            </div>
+          )}
+        </>
       ) : (
         <FullPageLoader />
       )}
