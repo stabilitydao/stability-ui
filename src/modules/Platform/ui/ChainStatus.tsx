5cdcab9f88145ec0c9def85ed5f00d181db3f817
devtealeaf <dev0tea0leaf@gmail.com>
2025-10-15 15:15:18 +0300

    upd: lib

--- src/modules/Platform/ui/ChainStatus.tsx
+++ src/modules/Platform/ui/ChainStatus.tsx
@@ -7,12 +7,6 @@ const ChainStatus: React.FC<TProps> = ({ status }) => {
   let text = "text-white";
   if (status === "SUPPORTED") {
     bg = "bg-green-800";
-  } else if (status === "AWAITING_DEPLOYMENT") {
-    bg = "bg-violet-800";
-  } else if (status === "AWAITING_ISSUE_CREATION") {
-    bg = "bg-orange-900";
-  } else if (status === "AWAITING_DEVELOPER") {
-    bg = "bg-yellow-700";
   }
 
   return (
@@ -20,11 +14,8 @@ const ChainStatus: React.FC<TProps> = ({ status }) => {
       className={`inline-flex text-[12px] px-3 py-1 rounded-2xl justify-center w-[160px] whitespace-nowrap ${bg} ${text}`}
     >
       {status
-        .replace("AWAITING_DEPLOYMENT", "Awaiting deployment")
         .replace("NOT_SUPPORTED", "Not supported")
-        .replace("SUPPORTED", "Supported")
-        .replace("AWAITING_ISSUE_CREATION", "Awaiting issue creation")
-        .replace("AWAITING_DEVELOPER", "Awaiting developer")}
+        .replace("SUPPORTED", "Supported")}
     </span>
   );
 };
