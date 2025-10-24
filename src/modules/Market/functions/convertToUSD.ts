2eea22fc271f73ef59450f35f3731aca2969015b
devtealeaf <dev0tea0leaf@gmail.com>
2025-10-24 18:51:47 +0300

    fix: formatNumber + convertToUSD

--- src/modules/Market/functions/convertToUSD.ts
+++ src/modules/Market/functions/convertToUSD.ts
@@ -2,8 +2,13 @@ import { formatNumber } from "@utils";
 
 export const convertToUSD = (value: string | number): string => {
   value = Number(value);
+
   if (!value) {
     return "";
+  }
+
+  if (value > 1000000000000) {
+    return "$1T+";
   }
 
   if (value < 1) {
