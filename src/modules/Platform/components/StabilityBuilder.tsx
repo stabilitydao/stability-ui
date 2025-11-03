2a4e8df3e80cbfcbf68b54cecb7754bdd32f66b8
Alien Deployer <m@a17.name>
2025-11-03 22:49:17 +0300

    🏗️🚧 Stability Builder: pools and conveyors skeleton

--- src/modules/Platform/components/StabilityBuilder.tsx
+++ src/modules/Platform/components/StabilityBuilder.tsx
@@ -1,6 +1,7 @@
 import {formatNumber} from "@utils";
 import {IBuilderAgent} from "@stabilitydao/stability/out/agents";
 import {AgentId, getAgent} from "@stabilitydao/stability";
+import {IConveyor, IPool} from "@stabilitydao/stability/out/builder";
 
 const StabilityBuilder  = (): JSX.Element => {
   const builderAgent: IBuilderAgent = getAgent("BUILDER" as AgentId) as IBuilderAgent;
@@ -12,10 +13,10 @@ const StabilityBuilder  = (): JSX.Element => {
 
       <div className="flex w-full flex-wrap gap-[20px] mb-[50px]">
 
-        <div className="flex flex-col rounded-xl lg:w-[300px]">
-          <div className="flex p-2 font-bold bg-accent-800">Burn rate</div>
-          <div className="flex p-2 text-[14px] bg-accent-900">
-            {builderAgent.burnRate.map(value => (
+        <div className="flex flex-col rounded-xl lg:w-[280px] bg-[#111114] border border-[#232429]">
+          <div className="flex p-[20px] font-bold rounded-t-xl bg-[#202027]">Burn rate</div>
+          <div className="flex p-[20px] text-[14px]">
+            {builderAgent.burnRate.map((value: IBuilderAgent) => (
               <div>
                 <table>
                   <tbody>
@@ -30,34 +31,57 @@ const StabilityBuilder  = (): JSX.Element => {
           </div>
         </div>
 
-        <div className="flex flex-col rounded-xl lg:w-[300px]">
-          <div className="flex p-2 font-bold bg-accent-800">Repositories</div>
-          <div className="flex p-2 flex-col bg-accent-900">
-            {builderAgent.repo.map(value => (
+        <div className="flex flex-col rounded-xl lg:w-[280px] bg-[#111114] border border-[#232429]">
+          <div className="flex p-[20px] font-bold rounded-t-xl  bg-[#202027]">Repositories</div>
+          <div className="flex p-[20px] flex-col">
+            {builderAgent.repo.map((value: string) => (
               <a title="Go to repo" target="_blank" href={`https://github.com/${value}`} className="flex text-[14px]">{value}</a>
             ))}
           </div>
         </div>
 
-        <div className="flex flex-col rounded-xl lg:w-[300px]">
-          <div className="flex p-2 font-bold bg-accent-800">Tokenization</div>
-          <div className="flex p-2 bg-accent-900">
+        <div className="flex flex-col rounded-xl lg:w-[280px] bg-[#111114] border border-[#232429]">
+          <div className="flex p-[20px] font-bold rounded-t-xl bg-[#202027]">Tokenization</div>
+          <div className="flex p-[20px]">
             {builderAgent.tokenization}
           </div>
         </div>
-
-
       </div>
 
-      <h2>Pools</h2>
-      <div className="flex w-full mb-[50px]">
+      <h2 className="text-[24px] font-bold mb-[10px]">Pools</h2>
+      <div className="flex w-full flex-wrap gap-[20px] mb-[50px]">
+        {builderAgent.pools.map((value: IPool) => (
+          <div className="flex flex-col rounded-xl lg:w-[280px] bg-[#111114] border border-[#232429]">
+            <div className="flex p-[20px] font-bold rounded-t-xl bg-[#202027]">{value.name}</div>
+            <div className="flex p-[20px]">
+              🚧
+            </div>
+          </div>
+        ))}
       </div>
 
-      <h2>Conveyors</h2>
-      <div className="flex w-full mb-[50px]">
+      <h2 className="text-[24px] font-bold mb-[10px]">Conveyors</h2>
+      <div className="flex flex-col w-full mb-[50px] gap-[20px]">
+        {builderAgent.conveyors.map((conveyor: IConveyor) => (
+          <div className="flex flex-col bg-[#111114] border border-[#232429]">
+            <div className="flex p-[20px] gap-[20px] font-bold bg-[#202027]">{conveyor.name}</div>
+            <div className="flex belt flex-nowrap justify-between  p-[20px]">
+              {conveyor.steps.map((step) => (
+                <div className="flex flex-col">
+                  <div>
+                    {step.name}
+                  </div>
+                  <div>
+                    🚧
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
 
-
+      <div className="flex mt-[100px]"> </div>
     </div>
   )
 }
