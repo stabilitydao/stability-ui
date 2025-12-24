5fd14e1300a6e5338f642e3c8ef6573dd6065aff
devtealeaf <dev0tea0leaf@gmail.com>
2025-12-24 16:44:16 +0200

    upd: stability lib + fixes

--- src/utils/functions/loadMarketsData.ts
+++ src/utils/functions/loadMarketsData.ts
@@ -24,13 +24,8 @@ const loadMarketsData = async (
     const apiMarket = markets?.[+chainId]?.[marketId];
 
     const reserves: TMarketReserve[] = libMarket.reserves.map((reserve) => {
-      // const backendData =
-      //   apiMarket?.reserves?.[reserve?.asset?.toLowerCase()] ?? {};
-
-      const backendData = apiMarket?.reserves?.find(
-        (_reserve) =>
-          _reserve?.aToken?.toLowerCase() === reserve?.aToken?.toLowerCase()
-      );
+      const backendData =
+        apiMarket?.reserves?.[reserve?.asset?.toLowerCase()] ?? {};
 
       return {
         address: reserve.asset,
